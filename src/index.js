df2836e8f7cc6c29c386f6a553e615646edb9a8e
Matt Travi <programmer@travi.org>
2018-05-10 01:12:21 -0500

    feat(rest-spread): added the transform

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import env from 'babel-preset-env';
+import restSpread from 'babel-plugin-transform-object-rest-spread';
 
 export default function () {
-  return {presets: [env(null, {targets: {node: 'current'}})]};
+  return {
+    presets: [env(null, {targets: {node: 'current'}})],
+    plugins: [[restSpread, {useBuiltIns: true}]]
+  };
 }
