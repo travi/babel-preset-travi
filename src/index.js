5010756d1e9f6ec54c8cd69fd4910d547f0a7b63
Matt Travi <programmer@travi.org>
2018-05-17 14:56:36 -0500

    feat(react): added an option to enabled react transpilation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import env from 'babel-preset-env';
 import restSpread from 'babel-plugin-transform-object-rest-spread';
 
-export default function () {
+export default function (context, {react}) {
   return {
-    presets: [env(null, {targets: {node: 'current'}})],
+    presets: [env(null, {targets: {node: 'current'}}), react && require('babel-preset-react')].filter(Boolean),
     plugins: [[restSpread, {useBuiltIns: true}]]
   };
 }
