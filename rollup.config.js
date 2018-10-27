063c120bc65a52052a9da6d20e9bc8b243dc089e
Matt Travi <programmer@travi.org>
2018-10-27 10:19:16 -0500

    build(rollup): enabled the auto-external plugin
    so that all all dependencies from node_modules are excluded from the bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
+import autoExternal from 'rollup-plugin-auto-external';
 
 export default {
   input: 'src/index.js',
-  external: ['babel-preset-env'],
+  plugins: [autoExternal()],
   output: [
     {file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true},
     {file: 'lib/index.es.js', format: 'es', sourcemap: true}
