e06dde0e1ecb51844a037887d96fef0926d4db15
Matt Travi <programmer@travi.org>
2018-11-10 00:27:50 -0600

    fix(babel-7): updated the export of preset-env

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import restSpread from '@babel/plugin-proposal-object-rest-spread';
 export default function (context, {react, targets = {}, modules} = {}) {
   return {
     presets: [
-      env(
-        null,
-        {
-          targets: {node: targets.node || 'current', ...targets.browser && {browsers: ['last 2 versions']}},
-          ...(false === modules) && {modules: false}
-        }
-      ),
+      [env, {
+        targets: {node: targets.node || 'current', ...targets.browser && {browsers: ['last 2 versions']}},
+        ...(false === modules) && {modules: false}
+      }],
       react && require('@babel/preset-react')
     ].filter(Boolean),
     plugins: [
